7b3ba4180bc3eaa0a2da47644e2388a4a718f85d
astr0 <astro@astr0s-MacBook-Pro.local>
2023-07-26 14:06:10 +0000

    Final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+        Lexend: ['Lexend Deca', 'sans-serif'],
+      },
+      colors: {
+        verydarkBlueMain:'hsl(233, 47%, 7%)',
+        Darkdesaturatedblue_card:'hsl(244, 38%, 16%)',
+        SoftViolet:'hsl(277, 64%, 61%)',
+        white:'hsl(0, 0%, 100%)',
+        slightlyTranspWhiteStat:'hsla(0, 0%, 100%, 0.75)',
+        slightlyTranspWhiteMain:'hsla(0, 0%, 100%, 0.6)',
+
+      }
+    },
   },
   plugins: [],
 }
